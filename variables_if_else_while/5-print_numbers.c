99a2e28639cb76401215e24a0cef8d8dcae69953
Sedra Ramarosaona <sedranante@gmail.com>
2025-06-18 14:32:55 +0000

    tache 5: boucle de zero a 10

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - all single digit numbers of base 10 starting from 0,
- * followed by a new line
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int singleDigit = 0;
+    int singleDigit = 0;
 
-	while (singleDigit < 10)
-	{
+    while (singleDigit < 10)
+    {
+        putchar(singleDigit + '0'); 
+        singleDigit++;
+    }
 
-		putchar(singleDigit);
-	}
+    putchar('\n');
 
-	singleDigit++;
-}
-putchar('\n');
-
-return (0);
+    return (0);
 }
