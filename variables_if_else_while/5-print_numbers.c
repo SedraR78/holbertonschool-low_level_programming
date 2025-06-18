20317f5a0b48bfee30fbc4f0ea4bdca34044238f
Sedra Ramarosaona <sedranante@gmail.com>
2025-06-18 14:10:55 +0000

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int singleDigit = 0;
+
+	while (singleDigit < 10)
+	{
+
+		putchar(singleDigit);
+	}
+
+	singleDigit++;
+}
+putchar('\n');
+
+return (0);
+}
