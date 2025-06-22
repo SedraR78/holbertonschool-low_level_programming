def3750e4ea7cee87fc751bc05fd16450999027a
Sedra Ramarosaona <sedranante@gmail.com>
2025-06-22 23:46:44 +0200

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Ajoute deux entiers.
+ * @a: Le premier entier.
+ * @b: Le second entier.
+ *
+ * Return: La somme des deux entiers.
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
