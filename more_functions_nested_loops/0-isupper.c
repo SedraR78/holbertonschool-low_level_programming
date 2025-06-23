c6270ffc72d0077347680bea008aa29c718f075c
Sedra Ramarosaona <sedranante@gmail.com>
2025-06-23 14:06:29 +0200

    Tache 1 : Check If a character is uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+        if( c >= 65  && c <= 90)
+        {
+                return(1);
+        }
+        else
+        {
+                return(0);
+        }
+        return(0);
+}
