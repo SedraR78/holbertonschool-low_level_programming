3f97d87b1a2b9e58856d71f5c6269e81705884c6
SedraR78 <sedranante@gmail.com>
2025-06-27 12:31:43 +0000

    Tache 4 : print in reserve

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
-
 void print_rev(char *s)
-{    char *finptr = s ;
-    while(*finptr != '\0')
-     {
-        finptr++;
-     }
-     if (finptr != s)
-     {
-        finptr++;
-     }
-     else
-     {
-        _putchar('\n');
-        return;
-     }
-     while (finptr >= s)
+{
+    int length = 0;
+    int i;
+
+    /* Calculate the length of the string */
+    while (s[length] != '\0')
     {
-        _putchar(*finptr); 
-        finptr--;          
+        length++;
     }
-    _putchar('\n'); 
+
+    /* Print the string in reverse */
+    for (i = length - 1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+
+    _putchar('\n'); /* Print a new line at the end */
 }
