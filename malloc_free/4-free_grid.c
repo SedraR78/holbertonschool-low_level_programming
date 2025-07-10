729c1bfb11d6533af2c37d83e4461275c2664256
SedraR78 <sedranante@gmail.com>
2025-07-10 12:30:48 +0000

    More Malloc Free , Création de main . h et README

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+
+void free_grid(int **grid, int height)
+{
+    if (grid == NULL)
+    {
+        return(NULL);
+    }
+    
+    for (int h= 0; h < height-1; h++)
+    {
+        if(grid[h] == NULL)
+        {
+            free(grid[h]);
+
+        }
+    }
+    free(grid);
+}
