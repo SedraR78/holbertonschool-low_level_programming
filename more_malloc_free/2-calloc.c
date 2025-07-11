fa7f257dafe5a60d57f4f242d107f7bc99710e3c
SedraR78 <sedranante@gmail.com>
2025-07-11 14:29:31 +0000

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,13 +3,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *output;
+    char *p;
 
     if (nmemb == 0 || size == 0)
         return(NULL);
-    output = calloc(nmemb, size);
-    if (output == NULL)
+    p = calloc(nmemb, size);
+    if (p == NULL)
         return (NULL);
     else   
-        return (output);
+        return (p);
 }
