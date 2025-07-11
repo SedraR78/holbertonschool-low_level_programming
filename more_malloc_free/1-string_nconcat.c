a2f484a3cff7992e55229ca98a1dbe4939caa758
SedraR78 <sedranante@gmail.com>
2025-07-11 09:46:53 +0000

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,10 +3,51 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int s1_taille = 0;
-    int s2_taille = 0;
-    int taille ;
-    int i; 
-    char *s;
+    unsigned int i;
+    unsigned int  s1_taille = 0;
+    unsigned int s2_taille = 0;
+    char *output;
+
+    if (s1 == NULL)
+            s1= "";
+    if (s2 == NULL)
+            s2 = "";
+    for ( i = 0; s1[i] != '\0'; i++)
+    {
+        s1_taille++;
+    }
+    for ( i = 0; s2[i]!= '\0'; i++)
+    {
+        s2_taille++;
+    }
+    
+    output=malloc(sizeof(char)* (s1_taille + n) + 1);
+    if (output == NULL)
+        return (NULL);
+    if (n >= s2_taille)
+    {
+        for ( i = 0; s1[i] != '\0'; i++)
+        {
+            output[i] = s1[i];
+        }
+        for ( i = 0; s2[i] != '\0'; i++)
+        {
+            output[s1_taille+i] = s2[i];
+        }
+    }
+    else
+    {
+        for ( i = 0; i != '\0' ; i++)
+        {
+            output[i] = s1[i];
+        }
+        for ( i = 0; i < n; i++)
+        {
+            output[s1_taille = i ] = s2[i];
+        }
+        output[s1_taille + i ] = '\0';
+    }
+    return(output);
+
 
 }
