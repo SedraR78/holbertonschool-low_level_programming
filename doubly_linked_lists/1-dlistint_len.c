1edd5c16b4aa84c9c714de35c748b4dd3c12199b
Sedra Ramarosaona <sedranante@gmail.com>
2025-08-01 10:25:35 +0200

    print the number of element of a linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t count = 0 ;
+
+        while(h){
+            count++; 
+            h =h->next;
+        }
+        return(count);
+}
